8e5d199c787202097b9a79f703de6ca6463038bf
Jollr <jolle.lont@gmail.com>
2015-12-27 11:01:16 +0100

    node setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+
+http.createServer(function (request, response) {
+	try {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end('test');
+	} catch(error) {
+		console.log(error);
+		response.writeHead(500, {'Content-Type': 'text/plain'});
+		response.end(error);
+	}
+	
+
+}).listen(80);
+
+console.log('Server running');
